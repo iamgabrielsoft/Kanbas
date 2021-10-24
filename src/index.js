dc4da8e21581f2e30a5f12907137a872a5b2fa6f
iamgabrielsoft <iamgabrielsoft@gmail.com>
2021-10-24 15:15:50 +0100

    add

--- src/index.js
+++ src/index.js
@@ -7,14 +7,14 @@ import "assets/styles/tailwind.css";
 
 // layouts
 
-import Admin from "layouts/Admin.js";
-import Auth from "layouts/Auth.js";
+import Admin from "./layouts/Admin";
+import Auth from "./layouts/Auth";
 
 // views without layouts
 
-import Landing from "views/Landing.js";
-import Profile from "views/Profile.js";
-import Index from "views/Index.js";
+import Landing from "./views/Landing";
+import Profile from "./views/Profile";
+import Index from "./views/Index";
 
 ReactDOM.render(
   <BrowserRouter>
